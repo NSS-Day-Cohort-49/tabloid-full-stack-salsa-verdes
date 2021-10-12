a1f7b78b63049facc5fe243a383add52e35dc6dd
Michael Trevino <mtrevin93@gmail.com>
2021-10-12 14:38:14 -0400

    start on tags

---
+++ Tabloid/Repositories/TagRepository.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
+using Tabloid.Models;
+using Tabloid.Utils;
+
+namespace Tabloid.Repositories
+{
+    public class TagRepository : BaseRepository, ITagRepository
+    {
+        public TagRepository(IConfiguration configuration) : base(configuration) { }
+        public List<Tag>Get()
+        {
+            using (var conn = Connection)
+            {
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = "SELECT * FROM Tag";
+                }
+                using (var reader)
+            }
+        }
+    }
