48493440cc056686eedc5ac8f1843b1d76125f88
dparsley1203 <dparsley1203@gmail.com>
2021-10-12 16:05:39 -0500

    added PostDetails to vscode.  Added GetbyId in post repo

---
+++ Tabloid/client/src/components/PostDetails.js
@@ -0,0 +1,45 @@
+import React, {useEffect, useState, UseState} from "react";
+import { ListGroup, ListGroupItem } from "reactstrap";
+import { useParams } from "react-router-dom";
+import { getPosts } from "../modules/postManager";
+
+
+export const PostDetails = () => {
+    const [post, setPost ] = useState({});
+    const { id } = useParams();
+
+
+    useEffect(() => {
+        getPosts(id)
+        .then(setPost);
+    })
+
+    return (
+        <div className="container">
+            <div className="row justify-content-center">
+                <div className="col-sm-12 col-lg-6">
+                    <Post post={post} />
+                    <ListGroup>
+                        {post.imageLocation}
+                        {post.content}
+                        {post.publishDateTime}
+                        {post.userProfile.userName}
+                    </ListGroup>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+
+// Given a user is viewing a list of Posts
+// When they select a post to read
+// Then they should be directed to a Post Detail page that shows the Post Details.
+
+// Post Details include:
+
+// Title
+// Header image (if exists)
+// Content
+// Publication date (MM/DD/YYYY)
+// Author's Display Name
