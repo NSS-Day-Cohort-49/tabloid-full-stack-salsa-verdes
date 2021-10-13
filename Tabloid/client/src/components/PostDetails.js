a590d143caecee1566ffb31cfd1a10c33af18666
dparsley1203 <dparsley1203@gmail.com>
2021-10-13 10:20:26 -0500

    formated details page some more

--- Tabloid/client/src/components/PostDetails.js
+++ Tabloid/client/src/components/PostDetails.js
@@ -9,6 +9,7 @@ export const PostDetails = () => {
     const [post, setPost ] = useState({});
     const { id } = useParams();
 
+    const date = new Date(post.publishDateTime).toDateString()
 
     useEffect(() => {
         getPostsId(id)
@@ -19,13 +20,14 @@ export const PostDetails = () => {
         <div className="container">
             <div className="row justify-content-center">
                 <div className="col-sm-12 col-lg-6">
-                    <Post post={post} />
-                    <ListGroup>
-                        {post.imageLocation}
-                        {post.content}
-                        {post.publishDateTime}
-                        {post.userProfile?.userName}
-                    </ListGroup>
+                    {/* <Post post={post} /> */}
+
+                        <img src={post.imageLocation} /> 
+                        <h2>{post.title}</h2>
+                        <div>{post.category?.name}</div>
+                        <div>{post.userProfile?.displayName} {date}</div><br></br>
+                        <p>{post.content}</p>
+
                 </div>
             </div>
         </div>
