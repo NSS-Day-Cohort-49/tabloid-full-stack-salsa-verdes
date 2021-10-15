b7213a62069b0fcc6c4e32331358571e6d899d8a
Michael Trevino <mtrevin93@gmail.com>
2021-10-15 05:11:56 -0400

    reduced clutter of conditional rendering statement in post

--- Tabloid/client/src/components/Post.js
+++ Tabloid/client/src/components/Post.js
@@ -72,32 +72,25 @@ const handleClickEditPost = () => {
     return (
         <Card className="">
             <CardBody>
-                {post.isByCurrentUser == true ? 
-                <div>
                 <Row>
-                    <Col>
+                <Col>
                     <Link to={`/post/${post.id}`}>
                     <strong>{post.title}</strong>
                      </Link>
                     {post.userProfile?.displayName}
                     {post.category?.name}
-                    </Col>
+                </Col>
+                {post.isByCurrentUser == true ? 
+                <>
                     <Col>
                         <Button onClick={handleClickEditPost}color="primary">Edit</Button>
                     </Col>
                     <Col>
                         <Button onClick={handleClickDeletePost}color="danger">Delete</Button>
                     </Col>
-                </Row>
-                </div>
-                 : 
-                 <Col>
-                 <Link to={`/post/${post.id}`}>
-                 <strong>{post.title}</strong>
-                  </Link>
-                 {post.userProfile?.displayName}
-                 {post.category?.name}
-                 </Col>}
+                </>
+                 : null}
+                </Row>    
             </CardBody>
         </Card>
     )
